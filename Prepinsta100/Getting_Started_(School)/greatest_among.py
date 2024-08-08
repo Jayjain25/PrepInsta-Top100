3de0b29200193716b3c7bacaed701fad045fb640
= <=>
2024-08-08 19:37:20 +0530

    Modify Greater Among

--- Prepinsta100/Getting_Started_(School)/greatest_among.py
+++ Prepinsta100/Getting_Started_(School)/greatest_among.py
@@ -1,16 +1,19 @@
-def Greater_among_two(num1,num2):
+def gretest_among_two(num1,num2):
   return max(num1,num2)
 
-def Greater_among_three(num1,num2,num3):
-  res = Greater_among_two(num1,num2)
-  return max(res,num3)
 
-def Greater_among_four(num1,num2,num3,num4):
-  res = Greater_among_three(num1,num2,num3)
-  return max(res,num4)
+def main():
+  try:
+    num1 = int(input("Enter a Number 1st :- "))
+    num2 = int(input("Enter a Number 2nd :- "))
+    
+    res = gretest_among_two(num1,num2)
+    
+    print(res,"is gretest")
+  
+  except Exception as e:
+    print(e)
+    
 
-def Greater_among_five(num1,num2,num3,num4,num5):
-  res = Greater_among_four(num1,num2,num3,num4)
-  return max(res,num5)
-
-print(Greater_among_four(2,10,1,20))
+if __name__ == "__main__":
+  main()
